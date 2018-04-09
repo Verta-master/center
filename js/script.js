0c7bce1fd5310b20797bf579bfc48fe92b558924
Verta-master <vertyagina@gmail.com>
2018-04-09 22:06:08 +0600

    Navigation

--- js/script.js
+++ js/script.js
@@ -16,3 +16,44 @@ $(window).scroll(function(){
     $(".menu").removeClass('menu--shadow');
   }
 });
+
+//Sidebar navigation
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('.sidemenu__item a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('sidemenu__link--active');
+        })
+        $(this).addClass('sidemenu__link--active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('.sidebar__item a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.sidemenu__item a').removeClass("sidemenu__link--active");
+            currLink.addClass("sidemenu__link--active");
+        }
+        else{
+//            currLink.removeClass("sidebar__link--active");
+        }
+    });
+}
