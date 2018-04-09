6e7a40040fa889c799a411f777543aecfae918de
Verta-master <vertyagina@gmail.com>
2018-04-09 20:50:46 +0600

    Update 2018-04-09T14:50:41.593Z

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ $('.menu__btn').click(function() {
   $('.menu__list').slideToggle();
   $('.menu').toggleClass('menu--opened');
 })
+
+//Header and menu on scroll
+$(window).scroll(function(){
+  if ((window.pageYOffset || document.documentElement.scrollTop ||document.body.scrollTop) >= 100) {
+    $(".header").addClass('header--shadow');
+    $(".header__wrap").addClass('header__wrap--scroll');
+    $(".menu").addClass('menu--shadow');
+  } else {
+    $(".header").removeClass('header--shadow');
+    $(".header__wrap").removeClass('header__wrap--scroll');
+    $(".menu").removeClass('menu--shadow');
+  }
+});
